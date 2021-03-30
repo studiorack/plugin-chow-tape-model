797b58c43394e688d335b983dce607686a1f36e3
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-03-29 19:10:47 -0700

    Adjust build script

--- Plugin/build_linux.sh
+++ Plugin/build_linux.sh
@@ -8,7 +8,7 @@ sed "s/_juce_target_/CHOWTapeModel/g" linux_utils/LV2.mak.in >LV2.mak
 # build Projucer
 (
     cd modules/DISTRHO-JUCE/extras/Projucer/Builds/LinuxMakefile
-    make
+    make -j4
 )
 
 PJ=./modules/DISTRHO-JUCE/extras/Projucer/Builds/LinuxMakefile/build/Projucer
@@ -20,7 +20,7 @@ echo "include ../../LV2.mak" >> Builds/LinuxMakefile/Makefile
 
 (
     cd Builds/LinuxMakefile
-    CONFIG=Release make
+    CONFIG=Release make Standalone VST3 -j4
 )
 
 rm LV2.mak
